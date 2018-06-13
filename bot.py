21db0ff94f41a903e4565d5fbf8f2c4b673b4b74
spinakker <friedl.mate@gmail.com>
2018-06-13 15:29:10 +0200

    removed fancy path validator function, might bite me in the ass

--- bot.py
+++ bot.py
@@ -42,36 +42,6 @@ alreadyPosted = set()
 
 user_agent = "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.7) Gecko/2009021910 Firefox/3.0.7"
 
-ERROR_INVALID_NAME = 123
-def is_pathname_valid(pathname: str) -> bool:
-
-    try:
-        if not isinstance(pathname, str) or not pathname:
-            return False
-
-        _, pathname = os.path.splitdrive(pathname)
-        root_dirname = os.environ.get('HOMEDRIVE', 'C:') \
-            if sys.platform == 'win32' else os.path.sep
-        assert os.path.isdir(root_dirname)
-        root_dirname = root_dirname.rstrip(os.path.sep) + os.path.sep
-        for pathname_part in pathname.split(os.path.sep):
-            try:
-                os.lstat(root_dirname + pathname_part)
-
-            except OSError as exc:
-                if hasattr(exc, 'winerror'):
-                    if exc.winerror == ERROR_INVALID_NAME:
-                        return False
-                elif exc.errno in {errno.ENAMETOOLONG, errno.ERANGE}:
-                    return False
-
-    except TypeError as exc:
-        return False
-
-    else:
-        return True
-
-
 def initializeConfig():
     with open('botconfig.json') as data_file:
         config_file = json.load(data_file)
@@ -119,9 +89,6 @@ def getPicsFromReddit(subs, picLimit):
         urllib.request.install_opener(myopener)
 
         if not filename.lower().endswith(('.png', '.jpg', '.jpeg')):
-            continue
-
-        if not is_pathname_valid(filename):
             continue
 
         md5 = createHash(submission.url)
