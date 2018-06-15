f60d8f4222a0a5228a1fc9f9bd02024f6fb012aa
spinakker <friedl.mate@gmail.com>
2018-06-16 01:53:12 +0200

    this is quite something tbh fam

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 
 # communication with the Discord API
 # config files
+import asyncio
 import json
 # obviously logging
 import logging
@@ -8,6 +9,7 @@ import logging
 import os
 # for a lot of things
 import random
+from enum import Enum
 
 # for bitcoin
 import aiohttp
@@ -16,6 +18,23 @@ from discord.ext import commands
 
 # my own
 from reddit import Reddit
+
+
+class PicturePostingTask:
+    class TimePerDay(Enum):
+        once = 1
+        twice = 2
+        thrice = 3
+
+    def __init__(self, subs, howOften, howMany, whichChannel):
+        self.subs = subs
+        self.timePerDay = howOften
+        self.numberOfPicturesToPost = howMany
+        self.targetChannel = whichChannel
+
+
+global picturePostingTasks
+picturePostingTasks = []
 
 global logger
 logger = logging.getLogger(__name__)
@@ -47,16 +66,6 @@ alreadyPosted = set()
 
 user_agent = "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.7) Gecko/2009021910 Firefox/3.0.7"
 
-with open('botconfig.json') as data_file:
-    config_file = json.load(data_file)
-
-global discordToken
-discordToken = config_file["discord"]["token"]
-
-### here we go
-global reddit
-reddit = Reddit('botconfig.json')
-bot.run(discordToken)
 
 
 @bot.event
@@ -82,19 +91,11 @@ async def bitcoin():
              pass_context=True)
 async def pics(context, subreddits="pics", limit=5):
     answerHelper = " pictures from **" + subreddits + "**."
-
-    submissions = reddit.client.subreddit(subreddits).hot(limit=50)
-    imagesPosted = 0
-    for submission in submissions:
-        filename = reddit.downloadImageFromSubmission(submission)
-        if filename:
-            await bot.send_file(context.message.channel, filename)
-            os.remove(filename)
-            imagesPosted = imagesPosted + 1
-        if imagesPosted == limit:
-            await bot.say(context.message.author.mention + " Found and posted " + str(imagesPosted) + answerHelper)
-            return
-    await bot.say(context.message.author.mention + " Only found " + str(imagesPosted) + answerHelper)
+    imagesPosted = await sendPics(limit, context.message.channel, subreddits)
+    if imagesPosted == limit:
+        await bot.say(context.message.author.mention + " Found and posted " + str(imagesPosted) + answerHelper)
+    else:
+        await bot.say(context.message.author.mention + " Only found " + str(imagesPosted) + answerHelper)
 
 
 @bot.command(name='8ball',
@@ -115,4 +116,72 @@ async def eight_ball(context):
     await bot.say(question + answer)
 
 
+async def sendPics(limit, channel, subreddits):
+    submissions = reddit.client.subreddit(subreddits).hot(limit=50)
+    imagesPosted = 0
+    for submission in submissions:
+        filename = reddit.downloadImageFromSubmission(submission)
+        if filename:
+            await bot.send_file(channel, filename)
+            os.remove(filename)
+            imagesPosted = imagesPosted + 1
+        if imagesPosted == limit:
+            break
+    return imagesPosted
 
+
+@bot.command(name='schedule',
+             pass_context=True)
+async def schedulePostingFromReddit(context, subs, limit=3, howOften='once'):
+    if howOften == 'once':
+        picturePostingTasks.append(
+            PicturePostingTask(subs, PicturePostingTask.TimePerDay.once, limit, whichChannel=context.message.channel))
+    elif howOften == 'twice':
+        picturePostingTasks.append(
+            PicturePostingTask(subs, PicturePostingTask.TimePerDay.twice, limit, whichChannel=context.message.channel))
+    elif howOften == 'thrice':
+        picturePostingTasks.append(
+            PicturePostingTask(subs, PicturePostingTask.TimePerDay.thrice, limit, whichChannel=context.message.channel))
+
+
+async def dailyTasks():
+    await bot.wait_until_ready()
+    while not bot.is_closed:
+        for task in picturePostingTasks:
+            if task.timePerDay == PicturePostingTask.TimePerDay.once:
+                await sendPics(task.numberOfPicturesToPost, task.targetChannel, task.subs)
+        await asyncio.sleep(86400)
+
+
+async def twelveHourTasks():
+    await bot.wait_until_ready()
+    while not bot.is_closed:
+        for task in picturePostingTasks:
+            if task.timePerDay == PicturePostingTask.TimePerDay.twice:
+                await sendPics(task.numberOfPicturesToPost, task.targetChannel, task.subs)
+        await asyncio.sleep(43200)
+
+
+async def eightHourTasks():
+    await bot.wait_until_ready()
+    while not bot.is_closed:
+        for task in picturePostingTasks:
+            if task.timePerDay == PicturePostingTask.TimePerDay.thrice:
+                await sendPics(task.numberOfPicturesToPost, task.targetChannel, task.subs)
+        await asyncio.sleep(28800)
+
+
+with open('botconfig.json') as data_file:
+    config_file = json.load(data_file)
+
+global discordToken
+discordToken = config_file["discord"]["token"]
+
+### here we go
+global reddit
+reddit = Reddit('botconfig.json')
+
+bot.loop.create_task(dailyTasks())
+bot.loop.create_task(twelveHourTasks())
+bot.loop.create_task(eightHourTasks())
+bot.run(discordToken)
