71f5559171b3455ed4e08e41dde4b56dce6db675
spinakker <friedl.mate@gmail.com>
2018-06-26 16:08:07 +0200

    header for aiohttp

--- utils.py
+++ utils.py
@@ -1,8 +1,10 @@
 
 async def download_image_from_url(url, file_path):
     import aiohttp
-    # TODO need headers to avoid 403
-    async with aiohttp.ClientSession() as session:
+    # TODO need to test if this avoids 403
+    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) '
+                             'Chrome/50.0.2661.102 Safari/537.36'}
+    async with aiohttp.ClientSession(headers=headers) as session:
         async with session.get(url) as resp:
             file_contents = await resp.read()
             with open(file_path, "wb") as f:
